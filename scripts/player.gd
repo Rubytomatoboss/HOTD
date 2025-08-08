a4393d66273e21bb9e18990dd91cce41d9eadd19
Rubytomatbososs <matricapac@gmail.com>
2025-08-09 00:10:02 +0300

    Добавил скрипт ходьбы и немного контента, котоырй я потом скорее всего удалю

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,21 @@
 extends CharacterBody2D
+class_name Player #сделаем игрока классом, чтобы в будущем было проще
+
+@export var speed: int
+@export var popup: PopupMenu
+@export var ray_cast:RayCast2D
+
+### движение
+func _physics_process(delta: float) -> void: #встроенная функция, которая происходит 60 раз в в секунду
+	velocity = speed * Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	move_and_slide()
+
+	ray_cast.target_position = get_global_mouse_position() 
+	
+	if ray_cast.collide_with_bodies:
+		print(ray_cast.get_collider(), get_global_mouse_position())
+		
+func _input(event: InputEvent) -> void:
+	if Input.is_key_pressed(KEY_E):
+		popup.position = get_viewport().get_mouse_position()
+		popup.show()
