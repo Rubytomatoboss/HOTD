d238a70f9e4d2f67146e5f948647904238fe90ae
Rubytomatbososs <matricapac@gmail.com>
2025-08-15 22:39:31 +0300

    удалил из кода упоминание чёрной рамки

--- scripts/object.gd
+++ scripts/object.gd
@@ -2,15 +2,9 @@ extends StaticBody2D
 
 @export var object_stats: UsableObject
 @onready var interactable: Area2D = $interactable
-@export var black_frame: Control
 
 func _ready() -> void:
 	interactable.interact = _on_interact
 	
 func _on_interact():
-	if black_frame.visible:
-		black_frame.hide()
-	else:
-		black_frame.show()
-		black_frame.upper_text = object_stats.name
-		black_frame.center_text = object_stats.description
+	pass
