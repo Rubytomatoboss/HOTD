a031ed4d5c567b9319cde1cdaea43224375fc92d
Rubytomatbososs <matricapac@gmail.com>
2025-08-17 22:56:21 +0300

    реализовываю механику исследования объектов

--- scripts/black_frame.gd
+++ scripts/black_frame.gd
@@ -1,13 +1,22 @@
 extends Control
 class_name BlackFrame
 
-var upper_text: String
-var center_text: String
+
+#var upper_text: String
+#var center_text: String
 
 @onready var upper_label: Label = $ColorRect/UpperLabel
 @onready var center_label: Label = $ColorRect/CenterLabel
 
 
-func _process(delta: float) -> void:
+#func _process(delta: float) -> void:
+	#upper_label.text = upper_text
+	#center_label.text = center_text
+
+
+func _on_object_show_black_frame(upper_text, center_text) -> void:
+	show()
 	upper_label.text = upper_text
 	center_label.text = center_text
+	print("lol")
+	
--- scripts/object.gd
+++ scripts/object.gd
@@ -1,7 +1,9 @@
 extends StaticBody2D
+signal showBlackFrame
 
 @export var object_stats: UsableObject
 @onready var interactable: Area2D = $interactable
+
 
 func _ready() -> void:
 	interactable.interact = _on_interact
@@ -12,3 +14,10 @@ func _on_interact(interaction):
 			print("used")
 		"examine":
 			print("examined")
+			emit_signal("showBlackFrame", object_stats.name, object_stats.description)
+			Engine.time_scale = 0
+			await press_to_continue()
+			Engine.time_scale = 1
+
+func press_to_continue():
+	pass
